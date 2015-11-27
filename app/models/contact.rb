926910bd8d851d1e717fa953242304d8497b3cb4
CC Evans <ccevans1@gmail.com>
2015-11-26 22:27:41 -0500

    update mobile footer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-     
+  attribute :signup,    :validate => true 
   attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
@@ -14,6 +14,6 @@ class Contact < MailForm::Base
     }
   end
 
-  BESTTIME = %w[Mornings Afternoons Evenings]
+  WEEKLY = %w[Daily Weekly]
 
 end
