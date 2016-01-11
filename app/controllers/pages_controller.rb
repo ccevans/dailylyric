6947f0a39d1e06e408b7c7879df8c9fd7a3fb8cd
cctheone <ccevans1@gmail.com>
2016-01-11 10:58:47 -0500

    update font size

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
-	
+
   def home
   	require 'will_paginate/array'
   	@lyrics = Lyric.includes(:artist, :song, :published_at, :status, :verse).published.order("created_at DESC")
   end
 
   def dash
-  	@all_posts = Lyric.all.order("created_at DESC")
+  	@all_posts = Lyric.all.order("published_at DESC").paginate(page: params[:page], per_page: 15)
   end
 end
